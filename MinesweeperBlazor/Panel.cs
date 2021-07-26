0cc53962d36d89b048f1fdcedb58b8788972de76
Viraaj Basi <viraajbasi@gmail.com>
2021-07-26 12:42:58 +0100

    created panel

---
+++ MinesweeperBlazor/Panel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinesweeperBlazor
+{
+    public class Panel
+    {
+        public int ID { get; set; }
+        //Horizontal position
+        public int X { get; set; }
+        //Vertical position
+        public int Y { get; set; }
+        public bool IsMine { get; set; }
+        public int AdjacentMines { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
+
+        public Panel(int id, int x, int y)
+        {
+            ID = id;
+            X = x;
+            Y = y;
+        }
+
+        public void Reveal()
+        {
+            IsRevealed = true;
+            IsFlagged = false; //Revealed panels cannot be flagged
+        }
+    }
+}
